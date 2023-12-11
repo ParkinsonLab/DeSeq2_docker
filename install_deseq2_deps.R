8111bca8658538925bfbd3d465976613df0c8580
BillyTaj <billyc59@gmail.com>
2023-12-11 18:01:51 -0500

    WIP: biocmanager needs updating.
    want 3.18, not 3.14.

---
+++ install_deseq2_deps.R
@@ -0,0 +1,5 @@
+install.packages("ggplot2", "~/storage/DeSeq2_docker/R_packages", dependencies = TRUE)
+install.packages("htmltools", "~/storage/DeSeq2_docker/R_packages", dependencies = TRUE)
+install.packages("BiocManager", "~/storage/DeSeq2_docker/R_packages", dependencies = TRUE)
+
+BiocManager::install("DESeq2")
---
+++ load_deseq2.sh
@@ -0,0 +1,1 @@
+singularity shell -B /home -B /usr/local /home/billy/storage/DeSeq2_docker/deseq2_docker_v1.0.0.sif
