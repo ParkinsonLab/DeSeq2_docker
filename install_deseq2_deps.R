59a88cb5b3c7504273d00e73052d2253278e5d49
billy <billyc59@gmail.com>
2023-12-13 04:12:46 -0500

    DESeq works!

--- install_deseq2_deps.R
+++ install_deseq2_deps.R
@@ -5,6 +5,7 @@ install.packages("~/storage/DeSeq2_docker/BiocManager_1.30.22.tar.gz", "~/storag
 
 library("BiocManager", lib.loc="~/storage/DeSeq2_docker/R_packages")
 
+
 BiocManager::install("BiocGenerics", lib="~/storage/DeSeq2_docker/R_packages")
 BiocManager::install("S4Vectors", lib="~/storage/DeSeq2_docker/R_packages")
 BiocManager::install("IRanges", lib="~/storage/DeSeq2_docker/R_packages")
@@ -17,17 +18,6 @@ BiocManager::install("Biobase", lib="~/storage/DeSeq2_docker/R_packages")
 BiocManager::install("DESeq2", lib="~/storage/DeSeq2_docker/R_packages")
 
 install.packages("matrixStats", "~/storage/DeSeq2_docker/R_packages", dependencies = TRUE)
+install.packages("memoise", "~/storage/DeSeq2_docker/R_packages", dependencies = TRUE)
 
-library("BiocGenerics", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("S4Vectors", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("IRanges", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("GenomeInfoDb", lib.loc="~/storage/DeSeq2_docker/R_packages")
 
-library("GenomicRanges", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("matrixStats", lib.loc="~/storage/DeSeq2_docker/R_packages")
-
-library("MatrixGenerics", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("Biobase", lib.loc="~/storage/DeSeq2_docker/R_packages")
-library("SummarizedExperiment", lib.loc="~/storage/DeSeq2_docker/R_packages")
-
-library("DESeq2", lib.loc="~/storage/DeSeq2_docker/R_packages")
--- load_deseq2.R
+++ load_deseq2.R
@@ -1,3 +1,14 @@
 library("BiocGenerics", lib.loc="~/storage/DeSeq2_docker/R_packages")
 library("S4Vectors", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("IRanges", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("GenomeInfoDb", lib.loc="~/storage/DeSeq2_docker/R_packages")
+
+library("GenomicRanges", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("matrixStats", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("Biobase", lib.loc="~/storage/DeSeq2_docker/R_packages")
+
+library("MatrixGenerics", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("SummarizedExperiment", lib.loc="~/storage/DeSeq2_docker/R_packages")
+library("memoise", lib.loc="~/storage/DeSeq2_docker/R_packages")
+
 library("DESeq2", lib.loc="~/storage/DeSeq2_docker/R_packages")
